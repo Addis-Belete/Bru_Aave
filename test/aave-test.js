2cbdadfb3614d1516787d8c839bc82a0abb3ba89
Addis Belete <addis.bel@gmail.com>
2022-05-26 16:01:48 +0300

    Install web3

--- test/aave-test.js
+++ test/aave-test.js
@@ -3,6 +3,8 @@ const { ethers } = require("hardhat");
 
 describe("Aave", function () {
 	it("Should depsit assets to Aave protocol and returns ADD token to user", async function () {
+		const WFTM = "";
+		const Dai = "0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E"
 		const ADD = await ethers.getContractFactory("addisToken");
 		const addisToken = await ADD.deploy();
 		await addisToken.deployed();
@@ -10,8 +12,10 @@ describe("Aave", function () {
 		const Aave = await ethers.getContractFactory("Aave");
 		const aave = await Aave.deploy(addisToken.address);
 		await aave.deployed();
-		const Swap = await ethers.getContractFactory("Swap")
 		console.log("Aave pool deployed too -->", aave.address);
+		const Swap = await ethers.getContractFactory("Swap");
+		const swap = await Swap.deploy();
+		await swap.deployed();
 		const daiWhale = "0x32276A3a773A06B97815338DfF6bcBcFD6bB4856";
 		await hre.network.provider.request({
 			method: "hardhat_impersonateAccount",
@@ -20,14 +24,14 @@ describe("Aave", function () {
 
 		const signer = await ethers.getSigner(daiWhale);
 		const DAI = new ethers.Contract(
-			"0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E",
+			Dai,
 			[
 				"function approve(address spender, uint256 amount) external returns (bool)",
 				"function balanceOf(address account) external view returns(uint256)"
 			], signer);
 
 		// swap fantom to dia;
-
+		await swap.convert()
 		const firstBal = await DAI.balanceOf(daiWhale);
 		console.log("Balance0:", ethers.utils.formatUnits(firstBal));
 
