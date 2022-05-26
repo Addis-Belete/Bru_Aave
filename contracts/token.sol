6ea9f5c00c1ebc2aae3607613a7b439c3f469793
Addis Belete <addis.bel@gmail.com>
2022-05-26 14:49:59 +0300

    Add a modifier

--- contracts/token.sol
+++ contracts/token.sol
@@ -1,23 +1,21 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract upDAI is ERC20, Ownable {
-    address private dv;
+    address aave;
 
-    constructor() ERC20("Addis Token", "ADD") {}
+    constructor(address _aave) ERC20("Addis Token", "ADD") {
+        aave = _aave;
+    }
 
-    modifier onlyVault() {
-        require(_msgSender() == address(dv), "Access denied");
+    modifier onlyAave() {
+        require(_msgSender() == aave, "Access denied");
         _;
     }
 
-    function mint(address _to, uint256 _amount) external onlyVault {
+    function mint(address _to, uint256 _amount) external onlyAave {
         _mint(_to, _amount);
-    }
-
-    function setVaultAddress(address _vault) external onlyOwner {
-        dv = _vault;
     }
 }
